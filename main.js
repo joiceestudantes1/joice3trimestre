9c3180eab84aa9ebc5743f0af677498ae38e6140
estudante alura <115159173+joiceestudantes1@users.noreply.github.com>
2023-10-26 09:00:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function tocaSomPom(){
+document.querySelector('som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').oniclik=tocaSomPom;
+
+
+function tocaSomclap(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').oniclik = tocaSomclap;
+
+function tocaSomtim() {
+    document.querySelector('#som_tecla_TIM).play();
+}
+
+function tocaSomtim() {
+    document.querySelector('#som_tecla_TIM).play();
+}s
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_Puff).play(); 
+}
+ 
